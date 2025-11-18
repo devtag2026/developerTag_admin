c7c9040265a18b073cef073c308a3f530f211bb8
developerAyesha <ayeshaakh234@gmail.com>
2025-11-18 11:25:47 +0500

    change the axiox configuration

--- src/config/ApiConfig.js
+++ src/config/ApiConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance
 const API = axios.create({
-    baseURL: "http://localhost:8000/api/v1", // Adjust the base URL if necessary
+    baseURL: process.env.NEXT_PUBLIC_API_URL || "https://developer-tag-backend-wz59.vercel.app/api/v1" // Adjust the base URL if necessary
 });
 
 API.interceptors.request.use(
